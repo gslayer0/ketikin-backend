20af34902d49e84f02e28091ef81a3ffe2b55659
bintangn <bintangnuralamsyah01@gmail.com>
2023-05-26 14:09:51 +0700

    add get and post method

--- main.go
+++ main.go
@@ -13,48 +13,71 @@ import (
 
 type PageData struct {
 	Writing string
+	Id      string
 }
 
 func main() {
 
-	db, err := sql.Open("mysql", "root:my-secret-pw@(127.0.0.1:3306)/ketikin")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	err = db.Ping()
+	db, err := sql.Open("mysql", "root:secret@(127.0.0.1:3306)/ketikin")
 	if err != nil {
 		panic(err.Error())
 	}
 
 	defer db.Close()
 
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
 	r := mux.NewRouter()
 	tmpl := template.Must(template.ParseFiles("public/index.html"))
 	r.HandleFunc("/{title}", func(w http.ResponseWriter, r *http.Request) {
 
-		req := mux.Vars(r)
-		noteId := req["title"]
+		switch {
+		case r.Method == "GET":
+			req := mux.Vars(r)
+			noteId := req["title"]
+			var (
+				id   string
+				teks string
+			)
+			query := `SELECT id, teks FROM note WHERE id = ?`
+			err := db.QueryRow(query, noteId).Scan(&id, &teks)
+			switch {
+			case err == sql.ErrNoRows:
+				_, err := db.Exec(`INSERT INTO note (id, teks) VALUES(?,?)`, noteId, "")
+				if err != nil {
+					panic(err.Error())
+				}
+				data := PageData{
+					Writing: "",
+					Id:      noteId,
+				}
+				tmpl.Execute(w, data)
 
-		var (
-			id   string
-			teks string
-		)
-
-		query := `SELECT id, teks FROM note WHERE id = ?`
-		err := db.QueryRow(query, noteId).Scan(&id, &teks)
-		if err != nil {
+			case err != nil:
+				panic(err.Error())
+			default:
+				data := PageData{
+					Writing: teks,
+					Id:      id,
+				}
+				tmpl.Execute(w, data)
+			}
+		case r.Method == "POST":
+			writing := r.FormValue("teks")
+			id := r.FormValue("id")
+			_, err := db.Exec(`UPDATE note set teks = ? where id = ?`, writing, id)
+			if err != nil {
+				panic(err.Error())
+			}
 			data := PageData{
-				Writing: err.Error(),
+				Writing: writing,
+				Id:      id,
 			}
 			tmpl.Execute(w, data)
-			return
 		}
-
-		data := PageData{
-			Writing: teks,
-		}
-		tmpl.Execute(w, data)
 	})
 	err = http.ListenAndServe(":8080", r)
 	if err != nil {
